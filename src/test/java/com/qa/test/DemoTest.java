17a71af8a9273905a8f50075de122a152c8c6103
RahulJain11 <rahuljaincs3@gmail.com>
2018-10-18 01:17:28 +0530

    my first commit

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,16 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void sum()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(a+b, 30);
+	}
+	
+}
